1b6657fc4ad98435299af518119d052c24a73389
nublius <184036331+nublius@users.noreply.github.com>
2025-09-05 18:07:42 -0700

    fix __repr__

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print('hello world')
+from textnode import TextNode, TextType
+
+def main():
+    dummy = TextNode('Anchor text', TextType.LINK, 'https://www.boot.dev')
+
+    print(dummy)
+
+if __name__ == "__main__":
+    main()
