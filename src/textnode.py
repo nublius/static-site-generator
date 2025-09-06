611f7137cba1fa043b9a62ab38c2f1899e9b0d81
nublius <184036331+nublius@users.noreply.github.com>
2025-09-05 18:24:50 -0700

    add unittest and implement edge case testing

--- src/textnode.py
+++ src/textnode.py
@@ -9,13 +9,18 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
     
-    def __eq__(self, text_node):
-        return self.__repr__ == text_node.__repr__
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+        return (self.text, self.text_type, self.url) == (
+            other.text, other.text_type, other.url
+        )
+
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
