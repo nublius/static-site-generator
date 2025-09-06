5017ceffd3ac16ac1cb6112b30fe89dd4f8c86c7
nublius <184036331+nublius@users.noreply.github.com>
2025-09-06 10:23:33 -0700

    work on parentnode to_html method

--- src/parentnode.py
+++ src/parentnode.py
@@ -3,3 +3,9 @@ from htmlnode import HTMLNode
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children == None:
+            raise ValueError("ParentNode must have children")
