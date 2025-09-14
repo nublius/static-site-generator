cec7c57666f064df4c171297b25f88f0401cf7d2
nublius <184036331+nublius@users.noreply.github.com>
2025-09-14 14:16:02 -0700

    test for split_nodes_image

---
+++ src/test_split_images_and_links.py
@@ -0,0 +1,22 @@
+import unittest
+from split_nodes_images_and_link import split_nodes_image
+from textnode import TextType, TextNode
+
+class TestSplitNodes(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
