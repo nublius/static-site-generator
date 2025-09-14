08de4fbfbe69e8aad2c34b4bdb3534a2e12f1800
nublius <184036331+nublius@users.noreply.github.com>
2025-09-14 16:39:33 -0700

    ran test

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,12 +1,4 @@
 def markdown_to_blocks(text):
     blocks = text.split("\n\n")
-    for block in blocks:
-        block = block.strip()
-
-    complete_blocks = []
-    for block in blocks:
-        if block:
-            complete_blocks.append(block)
-
-    return complete_blocks
+    return [b.strip() for b in blocks if b.strip()]
 
