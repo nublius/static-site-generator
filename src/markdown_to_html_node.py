9e60927da8931f070b07c525c48a1660b6c41dd6
nublius <184036331+nublius@users.noreply.github.com>
2025-09-23 23:08:35 -0700

    helper function text_to_children

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -2,3 +2,16 @@ from htmlnode import HTMLNode
 from markdown_to_blocks import markdown_to_blocks
 from extract_markdown import extract_markdown_images, extract_markdown_links
 from blocktype import BlockType, block_to_block_type
+from text_node_to_html_node import text_node_to_html_node
+from text_to_textnodes import text_to_textnodes
+
+def markdown_to_html_node(markdown):
+    split_markdown = markdown_to_blocks(markdown)
+    for block in split_markdown:
+        blocktype = block_to_block_type(block)
+        
+
+def text_to_children(text):
+    textnodes = text_to_textnodes(text)
+    return [text_node_to_html_node(tn) for tn in textnodes]
+
