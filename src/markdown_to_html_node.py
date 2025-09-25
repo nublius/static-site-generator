82ac64a3237a9ee864c073855e514bbaa3d81ec1
nublius <184036331+nublius@users.noreply.github.com>
2025-09-24 18:27:30 -0700

    helper functions for parsing ordered lists

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -1,9 +1,12 @@
+import re
 from htmlnode import HTMLNode
 from markdown_to_blocks import markdown_to_blocks
 from extract_markdown import extract_markdown_images, extract_markdown_links
 from blocktype import BlockType, block_to_block_type
 from text_node_to_html_node import text_node_to_html_node
 from text_to_textnodes import text_to_textnodes
+
+OL_MARKER = re.compile(r"^\d+\.\s")
 
 def markdown_to_html_node(markdown):
     split_markdown = markdown_to_blocks(markdown)
@@ -36,6 +39,34 @@ def parse_quote(block):
         else:
             new_lines.append(line)
     return "\n".join(new_lines)
+
+def parse_unordered_list(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if line.startswith("- "):
+            new_lines.append(line[2:])
+        elif line.startswith("-"):
+            new_lines.append(line[1:])
+        else:
+            new_lines.append(line)
+    return "\n".join(new_lines)
+
+def parse_ordered_list(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if is_ordered_item(line):
+            new_lines.append(strip_ordered_marker(line))
+        else:
+            new_lines.append(line)
+    return "\n".join(new_lines)
+
+def is_ordered_item(line):
+    return bool(OL_MARKER.match(line))
+
+def strip_ordered_marker(line):
+    return OL_MARKER.sub("", line, count=1)
 
 def trim_block_markers(block, blocktype):
     match blocktype:
